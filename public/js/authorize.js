b1ecc860593e21423f8a1b6c155059985d0f8bac
Glitch (trello-backup-restore) <none>
2025-03-15 09:36:24 +0000

    refactor

--- public/js/authorize.js
+++ public/js/authorize.js
@@ -1,26 +1,22 @@
 import { restorePopupCallback } from "/js/restore.js";
 
 var main = async function (t, withAuth) {
-  await t
-    .getRestApi()
-    .getToken()
-    .then(async function (token) {
-      if (/^[0-9a-fA-Z]{76}$/.test(token)) {
-        await t.popup({
-          callback: restorePopupCallback,
-          title: "Restore",
-          url: "/restore.html",
-          height: 40,
-        });
-      } else if (withAuth) {
-        await t.popup({
-          callback: authorizePopupCallback,
-          title: "Authorize",
-          url: "/authorize.html",
-          height: 40,
-        });
-      }
+  const token = await t.getRestApi().getToken();
+  if (/^[0-9a-fA-Z]{76}$/.test(token)) {
+    await t.popup({
+      callback: restorePopupCallback,
+      title: "Restore",
+      url: "/restore.html",
+      height: 40,
     });
+  } else if (withAuth) {
+    await t.popup({
+      callback: authorizePopupCallback,
+      title: "Authorize",
+      url: "/authorize.html",
+      height: 40,
+    });
+  }
 };
 
 var authorizePopupCallback = async function (t) {
