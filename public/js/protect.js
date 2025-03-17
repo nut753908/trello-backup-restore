dce8a26e4f0e998662b56cc885b19722bbfbd07b
Glitch (trello-backup-restore) <none>
2025-03-17 14:49:31 +0000

    fix function which disable buttons

--- public/js/protect.js
+++ public/js/protect.js
@@ -1,15 +1,17 @@
 export const protect = (func) => async (t) => {
+  window.onbeforeunload = () => {
+    t.set("member", "private", "busy", false);
+  };
+  if (await t.get("member", "private", "busy", false)) {
+    return;
+  }
+  await t.set("member", "private", "busy", true);
   try {
-    window.onbeforeunload = async () => {
-      await t.set("member", "private", "busy", false);
-    };
-    if (await t.get("member", "private", "busy", false)) {
-      return;
-    }
-    await t.set("member", "private", "busy", true);
-    await func(t);
+    await Promise.all([
+      new Promise((resolve) => setTimeout(resolve, 500)),
+      func(t),
+    ]);
   } finally {
-    await new Promise((resolve) => setTimeout(resolve, 500));
     await t.set("member", "private", "busy", false);
   }
 };
