e3f14d905ffaab6e6e297a26469ae4f928ee2012
Glitch (trello-backup-restore) <none>
2025-03-17 15:22:51 +0000

    refactor

--- public/js/protect.js
+++ public/js/protect.js
@@ -1,17 +1,19 @@
+const args = ["member", "private", "busyForBackupRestore"];
+
 export const protect = (func) => async (t) => {
   window.onbeforeunload = () => {
-    t.set("member", "private", "busyForBackupRestore", false);
+    t.set(...args, false);
   };
-  if (await t.get("member", "private", "busyForBackupRestore", false)) {
+  if (await t.get(...args, false)) {
     return;
   }
-  await t.set("member", "private", "busyForBackupRestore", true);
+  await t.set(...args, true);
   try {
     await Promise.all([
       new Promise((resolve) => setTimeout(resolve, 500)),
       func(t),
     ]);
   } finally {
-    await t.set("member", "private", "busyForBackupRestore", false);
+    await t.set(...args, false);
   }
 };
