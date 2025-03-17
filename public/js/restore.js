92727e4bd7538fc90b6056c8502e16848fd34443
Glitch (trello-backup-restore) <none>
2025-03-17 18:36:57 +0000

    refactor

--- public/js/restore.js
+++ public/js/restore.js
@@ -26,41 +26,47 @@ const cardKeys = [
   "coordinates",
 ];
 
-const restoreList = async (token, idBoard, file) => {
-  const text = await file.async("string");
-  const list = JSON.parse(text);
-  const name = list?.name;
-  const res = await createList(token, idBoard, name);
-  const resJson = await res.json();
-  return resJson?.id;
-};
-
-const restoreCard = async (token, idList, file) => {
+const restoreCard = async (file, token, idList) => {
   const text = await file.async("string");
   const card = JSON.parse(text);
   const body = cardKeys.reduce((o, k) => ({ ...o, [k]: card?.[k] }), {});
   const res = await createCard(token, idList, body);
-  const resJson = await res.json();
-  return resJson?.id;
+  const json = await res.json();
+  return json?.id;
+};
+
+const restoreList = async (file, token, idBoard) => {
+  const text = await file.async("string");
+  const list = JSON.parse(text);
+  const name = list?.name;
+  const res = await createList(token, idBoard, name);
+  const json = await res.json();
+  return json?.id;
+};
+
+const cardLoop = async (i, zip, token, idList) => {
+  const re = new RegExp(`^list${i}_card(\\d+)\\.json$`);
+  for (const file of zip.file(re)) {
+    await restoreCard(file, token, idList);
+  }
+};
+
+const listLoop = async (zip, token, idBoard) => {
+  const re = /^list(\d+)\.json$/;
+  for (const file of zip.file(re)) {
+    const idList = await restoreList(file, token, idBoard);
+    const i = file.name.match(re)[1];
+    await cardLoop(i, zip, token, idList);
+  }
 };
 
 const restore = (file) => async (t) => {
   t.alert({ message: "Restoring, please wait..." });
-  const token = await t.getRestApi().getToken();
-  const idBoard = t.getContext().board;
   const newZip = new JSZip();
   const zip = await newZip.loadAsync(file);
-  const listRe = /^list(\d+)\.json$/;
-  const lists = zip.file(listRe);
-  for (const list of lists) {
-    const idList = await restoreList(token, idBoard, list);
-    const i = list.name.match(listRe)[1];
-    const cardRe = new RegExp(`^list${i}_card(\\d+)\\.json$`);
-    const cards = zip.file(cardRe);
-    for (const card of cards) {
-      await restoreCard(token, idList, card);
-    }
-  }
+  const token = await t.getRestApi().getToken();
+  const idBoard = t.getContext().board;
+  await listLoop(zip, token, idBoard);
   t.alert({ message: "Restoration complete 🎉" });
 };
 
