4976bb547139533c09499b55d037e8c7a73b6bb3
Glitch (trello-backup-restore) <none>
2025-03-17 15:13:48 +0000

    rename busy key

--- public/js/protect.js
+++ public/js/protect.js
@@ -1,17 +1,17 @@
 export const protect = (func) => async (t) => {
   window.onbeforeunload = () => {
-    t.set("member", "private", "busy", false);
+    t.set("member", "private", "busyForBackupRestore", false);
   };
-  if (await t.get("member", "private", "busy", false)) {
+  if (await t.get("member", "private", "busyForBackupRestore", false)) {
     return;
   }
-  await t.set("member", "private", "busy", true);
+  await t.set("member", "private", "busyForBackupRestore", true);
   try {
     await Promise.all([
       new Promise((resolve) => setTimeout(resolve, 500)),
       func(t),
     ]);
   } finally {
-    await t.set("member", "private", "busy", false);
+    await t.set("member", "private", "busyForBackupRestore", false);
   }
 };
