337d9c702a3d63e6b238f1e6bb3c6e2a2212a0a2
Glitch (trello-backup-restore) <none>
2025-03-20 04:52:03 +0000

    move query params into body

--- public/js/restore/api.js
+++ public/js/restore/api.js
@@ -1,38 +1,46 @@
 import { APP_KEY } from "/js/env.js";
 
 export const createList = (token, idBoard, name) =>
-  fetch(
-    `https://api.trello.com/1/boards/${idBoard}/lists?name=${name}&pos=bottom&key=${APP_KEY}&token=${token}`,
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-      },
-    }
-  );
+  fetch(`https://api.trello.com/1/boards/${idBoard}/lists`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      pos: "bottom",
+      key: APP_KEY,
+      token,
+    }),
+  });
 
 export const createCard = (token, idList, body) =>
-  fetch(
-    `https://api.trello.com/1/cards?idList=${idList}&pos=bottom&key=${APP_KEY}&token=${token}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(body),
-    }
-  );
+  fetch(`https://api.trello.com/1/cards`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      ...body,
+      idList,
+      pos: "bottom",
+      key: APP_KEY,
+      token,
+    }),
+  });
 
 export const createAttachment = (token, idCard, body) =>
-  fetch(
-    `https://api.trello.com/1/cards/${idCard}/attachments?&key=${APP_KEY}&token=${token}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(body),
-    }
-  );
+  fetch(`https://api.trello.com/1/cards/${idCard}/attachments`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      ...body,
+      key: APP_KEY,
+      token,
+    }),
+  });
