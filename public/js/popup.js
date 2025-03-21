d6f3d93bc67d896abc2f987ca11e3712e3a6d628
Glitch (trello-backup-restore) <none>
2025-03-21 04:55:25 +0000

    refactor

--- public/js/popup.js
+++ public/js/popup.js
@@ -4,13 +4,13 @@ export const popupRestore = (t) =>
   t.popup({
     callback: restore,
     title: "Restore",
-    url: "/popup/restore.html",
+    url: "/restore.html",
     height: 40,
   });
 
 export const popupAuth = (t) =>
   t.popup({
     title: "Authorize",
-    url: "/popup/authorize.html",
+    url: "/authorize.html",
     height: 40,
   });
