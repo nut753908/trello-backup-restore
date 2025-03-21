7dbd04df327b147d19fb5f02d22ab63bcc51dd3d
Glitch (trello-backup-restore) <none>
2025-03-21 11:44:37 +0000

    fix message of start backup

--- public/js/back-up/back-up.js
+++ public/js/back-up/back-up.js
@@ -21,7 +21,7 @@ const download = (blob, name) => {
 };
 
 export const backUp = (type) => async (t) => {
-  t.alert({ message: `Backing up ${type}` });
+  t.alert({ message: `Backing up ${type}, please wait...` });
   const blob = await createZipBlob(t, type);
   const name = await createFilename(t, type);
   download(blob, name);
