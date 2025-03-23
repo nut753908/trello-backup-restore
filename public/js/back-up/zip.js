92ae2898bc37d482eadfe69855dc6a783e5409c0
Glitch (trello-backup-restore) <none>
2025-03-23 04:50:36 +0000

    add checklist as properties

--- public/js/back-up/zip.js
+++ public/js/back-up/zip.js
@@ -25,13 +25,15 @@ const getLists = {
 
 const addProperties = async (idBoard, token, lists) => {
   const res = await fetch(
-    `https://api.trello.com/1/boards/${idBoard}/cards?fields=cover&key=${APP_KEY}&token=${token}`
+    `https://api.trello.com/1/boards/${idBoard}/cards` +
+      `?fields=cover&checklists=all&key=${APP_KEY}&token=${token}`
   );
   const json = await res.json();
   const map = json.reduce((o, c) => ({ ...o, [c.id]: c }), {});
   lists.forEach((l) => {
     l.cards.forEach((c) => {
       c.cover = map[c.id].cover;
+      c.checklists = map[c.id].checklists;
     });
   });
 };
