16adf6a2514cdb94862ab8f69453aecd28d2e8a2
Glitch (trello-backup-restore) <none>
2025-03-24 16:26:57 +0000

    fix error handling

--- public/js/common/error.js
+++ public/js/common/error.js
@@ -1,8 +1,11 @@
 export const storeError = async (t, e) => {
-  const message = e.message ?? "";
   const stack = e.stack ?? "";
+  const message = e.message ?? "";
   const date = new Date().toISOString();
   const error = { message, stack, date };
+  if (stack.indexOf(message) !== -1) {
+    delete error.message;
+  }
   t.set("member", "private", "errorForBackupRestore", error);
   console.error(error);
 };
