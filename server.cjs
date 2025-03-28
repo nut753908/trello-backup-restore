5ee3b4f785bb13c1c38fd709cadd78403a40270e
nut <nut753908@gmail.com>
2025-03-28 20:23:49 +0900

    host https server when start

--- server.cjs
+++ server.cjs
@@ -1,7 +1,15 @@
+/* global process */
 const cors = require("cors");
 const express = require("express");
+const fs = require("fs");
+const https = require("https");
 
+const options = {
+  key: fs.readFileSync(`${process.env.HOME}/.sslcert/domain.key`),
+  cert: fs.readFileSync(`${process.env.HOME}/.sslcert/domain.crt`),
+  passphrase: process.env.PASSPHRASE,
+};
 const app = express();
 app.use(cors({ origin: "https://trello.com" }));
 app.use(express.static("public"));
-app.listen(3000);
+https.createServer(options, app).listen(8443);
