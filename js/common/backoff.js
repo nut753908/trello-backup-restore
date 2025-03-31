677f22288bb4bb660c7a66bf6de2cb3dda93050e
nut <nut753908@gmail.com>
2025-03-31 16:50:53 +0900

    support async to backoff

--- js/common/backoff.js
+++ js/common/backoff.js
@@ -1,25 +1,24 @@
-let attempt = 0;
+let i = -1;
 
 const fullJitter = () =>
   new Promise((resolve) =>
-    setTimeout(resolve, Math.min(32000, 1000 * 2 ** attempt) * Math.random())
+    setTimeout(resolve, Math.min(32000, 1000 * 2 ** i) * Math.random())
   );
 
 export const backoff = async (func) => {
+  i === -1 || (await fullJitter());
   const res = await func();
   if (res.status === 429) {
-    await fullJitter();
-    attempt++;
+    i++;
     return backoff(func);
   }
   if (
     res.headers.get("x-rate-limit-api-key-remaining") <= 100 ||
     res.headers.get("x-rate-limit-api-token-remaining") <= 50
   ) {
-    await fullJitter();
-    attempt++;
+    i++;
     return res;
   }
-  attempt = 0;
+  i = -1;
   return res;
 };
