c9cfdd7bfc3fc7b2033e8f55e7bc943ff1595879
nut <nut753908@gmail.com>
2025-03-31 15:38:06 +0900

    move place of console.error(e)

--- js/common/error.js
+++ js/common/error.js
@@ -1,7 +1,6 @@
 import { download } from "./file.js";
 
 export const storeError = async (t, e) => {
-  console.error(e);
   const message = e.message ?? "";
   const stack = e.stack ?? "";
   const date = new Date().toISOString();
